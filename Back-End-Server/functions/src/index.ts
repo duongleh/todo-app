612345b89d2f7bde0ea542e79e273b1d6805bfe8
duongle26 <leduong26298@gmail.com>
2019-08-11 18:50:08 +0700

    Add nodemon to watch ts file

--- Back-End-Server/functions/src/index.ts
+++ Back-End-Server/functions/src/index.ts
@@ -1,10 +1,10 @@
 import * as functions from 'firebase-functions';
 import app from "./app";
 
-// const PORT = 3000;
+const PORT = 3000;
 
-// app.listen(PORT, () => {
-//     console.log(`Server start on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server start on port ${PORT}`);
+});
 
 export const App = functions.https.onRequest(app);
